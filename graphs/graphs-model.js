ef668f8e0f29ee7c56fb0d65cd0a63faf204049f
micah riley <micahriley88@gmail.com>
2019-11-18 17:19:22 -0500

    Created export for knex model queries

---
+++ graphs/graphs-model.js
@@ -0,0 +1,17 @@
+const db = require('../database/dbConfig')
+
+module.exports = {
+    findGraphs,
+    findGraphById,
+    addGraph,
+    editGraph,
+//
+    findAreas,
+    findAreaById,
+    addArea,
+    editArea,
+//
+    findPoints,
+    addPoint,
+    editPoint
+};
