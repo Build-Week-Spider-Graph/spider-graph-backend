93b446a5f37104b5bdf453b3d2c70f9bbcc86aeb
micah riley <micahriley88@gmail.com>
2019-11-17 23:40:45 -0500

    Added basic registration and token-based authentication

--- api/server.js
+++ api/server.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const configureMiddleware = require('./conf-middleware')
 
-const graphRouter = require('../graphs/graphs-router')
 const authRouter = require('../auth/auth-router')
 
 const server = express();
 
 configureMiddleware(server);
 
-server.use('/api/graphs', graphRouter)
+
 server.use('/api/auth', authRouter)
 
 server.get('/', (req, res) => {
