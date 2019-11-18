09ed33f7b0dc26921425959352865db3b33b6b82
Connor R. Sullivan <crsullivan@protonmail.com>
2019-11-18 14:42:21 -0700

    added auth, token, routers

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,5 +1,59 @@
-// const router = require('express').Router();
+const router = require('express').Router();
 
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
+const Users = require('../users/users-model.js');
 
-// module.exports = router;
+router.post('/register', (req, res) => {
+  let user = req.body;
+
+  const hash = bcrypt.hashSync(user.password, 10);
+  user.password = hash;
+
+  Users.add(user)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+ 
+});
+
+router.post('/login', (req, res) => {
+  let { username, password } = req.body;
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = getJwtToken(user.username)
+        res.status(200).json({
+          message: `Welcome ${user.username}! have a token!!`,
+          token
+        });
+      } else {
+        res.status(401).json({ message: 'Invalid Credentials' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+function getJwtToken(username) {
+  const payload = {
+    username
+  };
+
+  const secret = process.env.JWT_SECRET || "BW-Mania!";
+
+  const options = {
+    expiresIn: "1d"
+  };
+
+  return jwt.sign(payload, secret, options);
+}
+
+module.exports = router;
